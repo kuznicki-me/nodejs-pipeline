7bcf23d292d28bdf4a287df98901fa6c10fb6f5e
Konrad Kuznicki <konrad@kuznicki.me>
2018-10-26 17:58:40 +0200

    htmltopdf

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,10 @@ WORKDIR /usr/src/app
 
 COPY package*.json ./
 
+RUN apt update
+
+RUN apt install poppler-utils -y
+
 RUN npm install
 
 COPY . .
--- index.js
+++ index.js
@@ -2,7 +2,55 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const host = '0.0.0.0';
+const spawn = require('child_process').spawn;
+const fs = require('fs');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/', (req, res) => {
+
+    const fc = fs.readFileSync('Clojure - Wikipedia.pdf');
+
+    //console.log(req)
+
+    pdf2xml(fc)
+        .then(buf => res.send(buf))
+        .catch((e) => {
+            console.log(e);
+            res.send('errrrrrrr...');
+        });
+
+
+});
+
+function pdf2xml(pdfContent) {
+    return new Promise((resolve, reject) => {
+        const pdftoxml = spawn(
+            'pdftohtml',
+            ['-xml', '-nodrm', '-i', '-nomerge', '-stdout', '-', 'void_file_name_to_satisfy_parser']); //, '-f', '0', '-l', '66'
+
+        let buf = '';
+        let errors = '';
+
+        pdftoxml.stderr.on('data', (data) => errors += data);
+        pdftoxml.stdout.on('data', (chunk) => buf += chunk);
+
+        pdftoxml.on('close', (code) => {
+            if (code !== 0 && errors) {
+                const message = 'could not parse PDF, error code: ' + code + ', errors: ' + errors;
+                if (buf.length > 0) {
+                    console.log(message);
+                    resolve(buf);
+                } else {
+                    reject(new Error(message));
+                }
+            } else {
+                resolve(buf);
+            }
+        });
+
+        pdftoxml.stdin.write(pdfContent);
+        pdftoxml.stdin.end();
+    });
+}
 
 app.listen(port, host, () => console.log(`Example app listening on port ${port}!`));
